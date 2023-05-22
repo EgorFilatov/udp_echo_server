1345481612f896fe6d8e65439fe0dab298a7b544
23egih23 <egor.filatov@live.com>
2023-05-22 15:57:56 +0300

    first commit

---
+++ Core/Inc/udpServerRAW.h
@@ -0,0 +1,14 @@
+#ifndef INC_UDPSERVERRAW_H_
+#define INC_UDPSERVERRAW_H_
+
+#include "lwip/pbuf.h"
+#include "lwip/udp.h"
+#include "lwip/tcp.h"
+
+#include "stdio.h"
+#include "udpServerRAW.h"
+
+void udpServer_init(void);
+
+
+#endif /* INC_UDPSERVERRAW_H_ */
